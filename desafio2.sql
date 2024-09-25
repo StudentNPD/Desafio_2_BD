7a14e59b5185b812b64212c1dfc5807375590f4e
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-09-25 18:06:27 -0300

    Pregunta 3 - Cuál o cuales son los registros de mayor antiguedad?

--- desafio2.sql
+++ desafio2.sql
@@ -1,3 +1,12 @@
+-- PREGUNTA 1 - 
 SELECT COUNT(cantidad) FROM inscritos;
-// 16 
+-- 16 
+
+-- PREGUNTA 2- 
+SELECT SUM(cantidad) FROM inscritos;
+-- 774 
+
+SELECT fecha, cantidad, fuente from inscritos WHERE fecha in (SELECT max(fecha) FROM inscritos);
+-- 2021-01-08  83 Blog
+-- 2021-01-08  99 Página
 
